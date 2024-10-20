6121347c62ca101dba3520a04393bdeed22f528a
Maksym Borshchev <m.borshchev@tempo.io>
2024-10-20 16:51:20 +0300

    Created start project and set up all necessary dependencies

---
+++ src/test/java/TreeProject/TreeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package TreeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
